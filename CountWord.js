8df1a98555e5ffaf4008c81e2e129608cbf1f4a1
KhoaDangPhamVu <116179021+KhoaDangPhamVu@users.noreply.github.com>
2023-08-16 09:59:54 +0700

    Count Word bro !!

---
+++ CountWord.js
@@ -0,0 +1,10 @@
+//Ý tưởng: Biến chuỗi thành array split(" ") --> Sau đó dùng array.length để đếm
+
+let countWord = function(text){
+    let count = 0;
+    let charArray = text.split(' ');
+    count = charArray.length;
+    console.log(`đoạn văn có ${count} từ`);
+}
+
+countWord("pham vu dang khoa")
